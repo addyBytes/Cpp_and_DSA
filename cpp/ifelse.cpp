6cabdb2f9f567321afdb1765b993756f93c418f1
Aditya Kapoor <141937170+addyBytes@users.noreply.github.com>
2025-08-14 21:47:22 +0530

    Add files via upload

---
+++ cpp/ifelse.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+  int age;
+  cout<< "enter your age"<<endl;
+  cin>>age;
+  if(age>=18){
+    cout<< "you are an adult";
+
+  }
+  else{
+    cout<<"you are not an adult";
+  }
+
+  return 0;
+}
+
