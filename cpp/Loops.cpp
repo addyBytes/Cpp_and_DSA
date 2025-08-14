d4091450870e48a9a7686559cf736c96f301d4ca
Aditya Kapoor <141937170+addyBytes@users.noreply.github.com>
2025-08-14 22:07:36 +0530

    Loops cpp added

---
+++ cpp/Loops.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int a = 1 ;
+
+while(a<14){
+	cout<<a+1<<endl;
+	a++;
+}
+
+//do-while Works atleast one time
+
+do{
+	cout<<a+1<<endl;
+	a++;
+}while(a<5);
+
+//for loop
+
+for(int i =0 ; i<10 ; i++){
+	
+	cout<<i;
+}
+}
