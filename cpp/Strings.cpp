d35d1be5f53252f097ae108914aa50c4884982d0
Aditya Kapoor <141937170+addyBytes@users.noreply.github.com>
2025-08-15 11:24:42 +0530

    Strings added

---
+++ cpp/Strings.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string name = "Aditya";
+	
+	cout<<"the string is "<<name<<endl;
+	cout<<"the length is "<<name.length()<<endl;
+	cout<<"the sub string is "<<name.substr(0,3)<<endl;
+	
+	return 0;
+}
